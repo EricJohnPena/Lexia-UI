d2d90a8107a74e05624b3ae403e2fdb3fe0f7cd8
EricJohnPena <119671372+EricJohnPena@users.noreply.github.com>
2024-12-03 11:32:28 +0800

    connected ui

---
+++ Assets/Scripts/MenuScripts/MenuManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager InstanceMenu;
+    private void Awake(){
+        InstanceMenu = this;
+    }
+
+    [SerializeField] Button goToLoginBtn;
+    [SerializeField] Button goToSignupBtn;
+    //[SerializeField] Button loginBtn;
+    [SerializeField] Button goToLoginBtn1; //from signup page
+    [SerializeField] Button goToSignupBtn1; //from login page
+   
+
+    
+
+
+    public Canvas StartingPage;
+    public Canvas LoginPage;
+    public Canvas SignupPage;
+
+   
+   void Start(){
+    goToLoginBtn.onClick.AddListener(() => OpenPage(LoginPage));
+    goToLoginBtn1.onClick.AddListener(() => OpenPage(LoginPage));
+    goToSignupBtn.onClick.AddListener(() => OpenPage(SignupPage));
+    goToSignupBtn1.onClick.AddListener(() => OpenPage(SignupPage));
+    //loginBtn.onClick.AddListener(ToMenuScene);//for testing purposes only
+
+   }
+
+   public void OpenPage(Canvas canvas){
+    StartingPage.gameObject.SetActive(false);
+    LoginPage.gameObject.SetActive(false);
+    SignupPage.gameObject.SetActive(false);
+    canvas.gameObject.SetActive(true);
+   }
+
+    void ToMenuScene(){
+        SceneController.Instance.GoToMainMenu(); //
+    }
+
+
+}
---
+++ Assets/Scripts/MenuScripts/SceneController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{ 
+    public static SceneController Instance { get; private set; }
+   void Awake() {
+    if (Instance == null) {
+        Instance = this;
+        
+    }
+}
+    public enum Scene{
+        LoginScene,
+        MenuScene, 
+        LoadingScene,
+        GameScene
+    }
+    
+    public void GoToScene(Scene sceneName)
+    {
+        SceneManager.LoadScene(sceneName.ToString());
+      
+    }
+    public void GoToStartingScene(){
+        SceneManager.LoadScene(Scene.LoginScene.ToString());
+    }
+
+    public void GoToMainMenu(){
+        SceneManager.LoadScene(Scene.GameScene.ToString());
+        
+    }
+
+    public void CreateNewGame(){
+        SceneManager.LoadScene(Scene.GameScene.ToString());
+        
+     }
+}
